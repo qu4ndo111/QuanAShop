3ca66c6127fc24bef376d34bf26f010a7de9f11e
qu4ndo111 <qu4ndo111@gmail.com>
2022-12-08 20:14:28 +0700

    modified:   pages/success.js

--- pages/success.js
+++ pages/success.js
@@ -1,19 +1,41 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useEffect, useContext } from 'react'
 import Link from 'next/link'
 import { BsBagCheckFill } from 'react-icons/bs'
-import { runFireworks } from '../lib/utils'
-
+import confetti from "canvas-confetti";
 import { Context } from '../context/StateContext'
 
 const Success = () => {
     const useStateContext = useContext(Context)
     const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext
-   
+
     useEffect(() => {
         setCartItems([])
         setTotalPrice(0)
         setTotalQuantities(0)
-        runFireworks()
+        var end = Date.now() + (3 * 1000);
+
+        // go Buckeyes!
+        var colors = ['#bb0000', '#ffffff'];
+        (function frame() {
+            confetti({
+                particleCount: 2,
+                angle: 60,
+                spread: 55,
+                origin: { x: 0 },
+                colors: colors
+            });
+            confetti({
+                particleCount: 2,
+                angle: 120,
+                spread: 55,
+                origin: { x: 1 },
+                colors: colors
+            });
+
+            if (Date.now() < end) {
+                requestAnimationFrame(frame);
+            }
+        }())
     }, [])
 
     return (
@@ -21,14 +43,14 @@ const Success = () => {
             <div className='success'>
                 <p className='icon'>
                     <BsBagCheckFill />
-                </p> 
+                </p>
                 <h2>Thank you for your order!</h2>
                 <p className='email-msg'>Check your email inbox for the receipt.</p>
                 <p className='description'>
                     If you have any questions, please email <a className='email' href='mailto:shopquana@test.com'>
-                         shopquana@test.com
+                        shopquana@test.com
                     </a>
-                    
+
                 </p>
                 <Link href='/' legacyBehavior>
                     <button type='button' width='300px' className='btn'>
