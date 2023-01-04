ea2d7a603b0dfbdcc552428d0a197f505306ccb3
qu4ndo111 <qu4ndo111@gmail.com>
2023-01-04 09:54:31 +0700

    modified:   pages/product/[slug].js 	modified:   styles/globals.css

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -48,7 +48,7 @@ const ProductDetails = ({ product, products }) => {
         if (user && user[0].avatar) {
             return user[0].avatar
         } else if (!user) {
-            
+
         }
     }
 
@@ -82,7 +82,7 @@ const ProductDetails = ({ product, products }) => {
         event.preventDefault()
         setLoading(true)
         client.patch(_id).setIfMissing({ comment: [] }).insert('after', 'comment[-1]', [{ _key: nanoid(), name: setName(), comment: reviewData.comment, datetime: datetime, avatar: userAvatar() }]).commit().then(data => {
-           setUserComments(data.comment)
+            setUserComments(data.comment)
         }).then(() => {
             setReviewData(prev => {
                 return {
@@ -116,131 +116,137 @@ const ProductDetails = ({ product, products }) => {
 
     return (
         <div>
-            <div className='product-detail-container'>
-                <div>
-                    <div className='product-detail-image'>
-                        <img src={urlFor(image && image[index])}
-                            className='product-detail-image'
-                        />
-                    </div>
-                    <div className='small-image-container'>
-                        {image?.map((item, i) => (
-                            <img
-                                key={i}
-                                src={urlFor(item)}
-                                className={i === index ? 'small-image selected-image' : 'small-image'}
-                                onMouseEnter={() => setIndex(i)}
+            {product ? <div>
+                <div className='product-detail-container'>
+                    <div>
+                        <div className='product-detail-image'>
+                            <img src={urlFor(image && image[index])}
+                                className='product-detail-image'
                             />
-                        ))}
-                    </div>
-                </div>
-                <div className='product-content-container'>
-                    <div className='product-detail-desc'>
-                        <h1>{name}</h1>
-                        <div className='reviews'>
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiFillStar />
-                            <AiOutlineStar />
-                            <p>({userComments ? userComments.length : 0})</p>
+                        </div>
+                        <div className='small-image-container'>
+                            {image?.map((item, i) => (
+                                <img
+                                    key={i}
+                                    src={urlFor(item)}
+                                    className={i === index ? 'small-image selected-image' : 'small-image'}
+                                    onMouseEnter={() => setIndex(i)}
+                                />
+                            ))}
                         </div>
                     </div>
-                    <h4>Details: </h4>
-                    <p>{details}</p>
-                    <p className='price'>${price}</p>
-                    <div className='quantity'>
-                        <h3>Quantity: </h3>
-                        <p className='quantity-desc'>
-                            <span className='minus'
-                                onClick={decQty}>
-                                <AiOutlineMinus />
-                            </span>
-                            <span className='num'
-                            >
-                                {qty}
-                            </span>
-                            <span className='plus'
-                                onClick={incQty}>
-                                <AiOutlinePlus />
-                            </span>
-                        </p>
-                    </div>
-                    <div className='buttons'>
-                        <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>
-                            Add to Cart
-                        </button>
-                        <button type='button' className='buy-now' onClick={handleBuyNow}>
-                            Buy now
-                        </button>
+                    <div className='product-content-container'>
+                        <div className='product-detail-desc'>
+                            <h1>{name}</h1>
+                            <div className='reviews'>
+                                <AiFillStar />
+                                <AiFillStar />
+                                <AiFillStar />
+                                <AiFillStar />
+                                <AiOutlineStar />
+                                <p>({userComments ? userComments.length : 0})</p>
+                            </div>
+                        </div>
+                        <h4>Details: </h4>
+                        <p>{details}</p>
+                        <p className='price'>${price}</p>
+                        <div className='quantity'>
+                            <h3>Quantity: </h3>
+                            <p className='quantity-desc'>
+                                <span className='minus'
+                                    onClick={decQty}>
+                                    <AiOutlineMinus />
+                                </span>
+                                <span className='num'
+                                >
+                                    {qty}
+                                </span>
+                                <span className='plus'
+                                    onClick={incQty}>
+                                    <AiOutlinePlus />
+                                </span>
+                            </p>
+                        </div>
+                        <div className='buttons'>
+                            <button type='button' className='add-to-cart' onClick={() => onAdd(product, qty)}>
+                                Add to Cart
+                            </button>
+                            <button type='button' className='buy-now' onClick={handleBuyNow}>
+                                Buy now
+                            </button>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='maylike-products-wrapper'>
-                <h2>You may also like</h2>
-                <div className='marquee'>
-                    <div className='maylike-products-container'>
-                        {similarProduct.slice(0, 6).map((item) => (
-                            <Product key={item._id}
-                                product={item} />
-                        ))}
+                <div className='maylike-products-wrapper'>
+                    <h2>You may also like</h2>
+                    <div className='marquee'>
+                        <div className='maylike-products-container'>
+                            {similarProduct.slice(0, 6).map((item) => (
+                                <Product key={item._id}
+                                    product={item} />
+                            ))}
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div>
-                <form className='user-review-container' onSubmit={HandleSubmitComment}>
-                    <h2>Review product</h2>
-                    <input
-                        type='text'
-                        placeholder={displayUserName()}
-                        onChange={HandleChangeComment}
-                        value={value()}
-                        name='name'
-                        required
+                <div>
+                    <form className='user-review-container' onSubmit={HandleSubmitComment}>
+                        <h2>Review product</h2>
+                        <input
+                            type='text'
+                            placeholder={displayUserName()}
+                            onChange={HandleChangeComment}
+                            value={value()}
+                            name='name'
+                            required
 
-                    />
-                    <textarea
-                        placeholder='Enter your comment here'
-                        onChange={HandleChangeComment}
-                        value={reviewData.comment}
-                        name='comment'
-                        required
-                    />
-                    <div className='form-button'>
-                        <label htmlFor='image-upload' className='button-upload'>
-                            <input
-                                type='file'
-                                id='image-upload'
-                                multiple accept="image/*"
-                            />
-                            <AiFillCamera size={30} />
-                        </label>
-                        <button type='submit'>Post comment</button>
-                    </div>
-                    <div>
-                        {!loading ? <div>
-                            {userComments?.map(data => (
-                                data.name && <div className='feedback' key={data._key}>
-                                    {commentAvatar(data.avatar)}
-                                    <div className='user-box'>
-                                        <h3>{data.name}</h3>
-                                        <div className='user-comment-box'>
-                                            <p className='user-comment'>{data.comment}</p>
-                                            <p className='user-date'>{data.datetime}</p>
+                        />
+                        <textarea
+                            placeholder='Enter your comment here'
+                            onChange={HandleChangeComment}
+                            value={reviewData.comment}
+                            name='comment'
+                            required
+                        />
+                        <div className='form-button'>
+                            <label htmlFor='image-upload' className='button-upload'>
+                                <input
+                                    type='file'
+                                    id='image-upload'
+                                    multiple accept="image/*"
+                                />
+                                <AiFillCamera size={30} />
+                            </label>
+                            <button type='submit'>Post comment</button>
+                        </div>
+                        <div>
+                            {!loading ? <div>
+                                {userComments?.map(data => (
+                                    data.name && <div className='feedback' key={data._key}>
+                                        {commentAvatar(data.avatar)}
+                                        <div className='user-box'>
+                                            <h3>{data.name}</h3>
+                                            <div className='user-comment-box'>
+                                                <p className='user-comment'>{data.comment}</p>
+                                                <p className='user-date'>{data.datetime}</p>
+                                            </div>
                                         </div>
                                     </div>
+                                ))}
+                            </div> : (
+                                <div className='Loading'>
+                                    <Spinner />
                                 </div>
-                            ))}
-                        </div> : (
-                            <div className='Loading'>
-                                <Spinner />
-                            </div>
-                        )
-                        }
-                    </div>
-                </form>
-            </div>
+                            )
+                            }
+                        </div>
+                    </form>
+                </div>
 
+            </div> : (
+                <div className='Loading'>
+                    <Spinner />
+                </div>
+            )}
         </div>
     )
 }
