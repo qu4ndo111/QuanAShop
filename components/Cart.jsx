42f1b0063d66b78a633b209e4a906b69bc40b965
qu4ndo111 <qu4ndo111@gmail.com>
2023-02-01 14:16:46 +0700

    modified:   components/Cart.jsx

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -34,7 +34,7 @@ const Cart = () => {
 
   return (
     <div className='cart-wrapper' ref={cartRef} >
-      <div className='cart-container' >
+      <div className='cart-container'>
         <button type='button'
           className='cart-heading'
           onClick={() => setShowCart(false)}
